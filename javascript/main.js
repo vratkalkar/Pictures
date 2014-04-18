42523ce1b90f248b2787acdabd3092a430808202
Vivek <vratkalkar@live.com>
2014-04-18 17:53:15 -0400

    added ajax js function

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
 /* CURRENTLY IN: javascript/main.js */
+
+$.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/3364414/media/recent/?access_token=3364414.691e182.dc90822dd2c34d5aaf4cccae7bbf4974",
+        success: function(data) {...}
+    });
+});
